99ba1613526894646282e4d5ee0a486c83293ac6
Drew Franz <drewfranz@gmail.com>
2021-01-22 14:04:29 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import * as quotedJson from "./data/quotes.json";
+
+import './App.css';
+
+const Quote = (props) => {
+  return (
+    <div className="quote-container">
+      <div className="quote-body">{props.quote.text}</div>
+      <div className="quote-author">{props.quote.author}</div>
+    </div>
+  );
+}
+
+export default function App() {
+  const [quotes, setQuotes] = useState([{}]);
+
+  useEffect(() => {
+    setQuotes(quotedJson.quotes);
+  }, [setQuotes])
+
+  return (
+    <div className="App">
+      <Quote quote={quotes[Math.floor(Math.random() * quotes.length)]} />
+    </div>
+  );
+};
