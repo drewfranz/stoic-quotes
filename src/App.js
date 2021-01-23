e11c7a7f53077c09a4b4437c6d02d4a98c48134c
Drew Franz <drewfranz@gmail.com>
2021-01-22 20:48:02 -0800

    Add refresh handler

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import { useState, useEffect } from 'react';
 import * as quotedJson from "./data/quotes.json";
+import { IconContext } from "react-icons";
+import { AiOutlineReload } from 'react-icons/ai';
+
 
 import './App.css';
 
 const Quote = (props) => {
   return (
-    <div className="quote-container">
+    <div className="quote-container" onClick={() => props.handler()}>
       <div className="quote-body">{props.quote.text}</div>
       <div className="quote-author">{props.quote.author}</div>
     </div>
   );
 }
 
+const Refresh = (props) => {
+  return (
+    <>
+      <button className="btn button-refresh" onClick={() => props.handler()}>
+        <IconContext.Provider value={{
+          color: "#666",
+          size: "2rem",
+          className: "button-refresh-icon" }}>
+          <AiOutlineReload />
+        </IconContext.Provider>
+      </button>
+    </>
+  );
+}
+
 export default function App() {
-  const [quotes, setQuotes] = useState([{}]);
+  const [selected, setSelected] = useState({})
 
   useEffect(() => {
     document.title = "Stoic Quotes";
-    setQuotes(quotedJson.quotes);
-  }, [setQuotes])
+    setSelected(quotedJson.quotes[Math.floor(Math.random() * quotedJson.quotes.length)]);
+  }, [setSelected]);
+
+  const handleRefresh = () => {
+    setSelected(quotedJson.quotes[Math.floor(Math.random() * quotedJson.quotes.length)]);
+  }
 
   return (
     <div className="App">
-      <Quote quote={quotes[Math.floor(Math.random() * quotes.length)]} />
+      <Quote quote={selected} handler={handleRefresh} />
+      <Refresh handler={handleRefresh} />
     </div>
   );
 };
